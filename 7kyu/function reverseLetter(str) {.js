b289a698e4cf9db96a978d636981f30e0bdebcba
Luiggy Macias <ferrinluiggy@gmail.com>
2021-05-31 13:58:11 -0500

    updated

---
+++ 7kyu/function reverseLetter(str) {.js
@@ -0,0 +1,12 @@
+/*function reverseLetter(str) {
+    //coding and coding..
+    let rts = str.match(/[a-z]/g).reverse().join('')
+    console.log(rts)
+    return rts
+
+}*/
+
+function reverseLetter(str) {
+    return str.match(/[a-z]/g).reverse().join('')
+}
+console.log(reverseLetter("krishan"))
