ea020b915ae96b0d72c2319045fcef8cb6478555
macluiggy <ferrinluiggy@gmail.com>
2021-11-12 10:06:36 -0500

    updated

--- 6 kyu/Playing with digits.js
+++ 6 kyu/Playing with digits.js
@@ -1,8 +1,18 @@
 function digPow(n, p){
   // ...
   let arr = n.toString().split('')
-  let arr2 = arr.reduce((sum, c, i) => sum + c**(p+i), 0)
-  return arr2
+  let n2 = arr.reduce((sum, c, i) => sum + c**(p+i), 0)
+  k = 0;
+  let r;
+  while (n*k !== n2 && n*k < n2) {
+    k++
+    if (n*k === n2) {
+      r = k
+    } else {
+      r = -1
+    }  
+  }
+  return r
 }
 
-console.log(digPow(89, 1))
+console.log(digPow(46288, 3))
---
+++ 6 kyu/Playing with digits.py
@@ -0,0 +1,12 @@
+def digPow(n, p):
+  arr = str(n)
+  arr2 = arr.split(' ')
+  print(arr2)
+  return arr
+
+print(digPow(46288, 3))
+
+a = ''
+print(a)
+a=3
+print(a)
