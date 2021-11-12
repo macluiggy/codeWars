3eb9b3a8b8174a21fe20e6ac3f3d188c374f80b3
macluiggy <ferrinluiggy@gmail.com>
2021-11-12 15:54:16 -0500

    updated

--- TypeScript/8 kyu/Basic subclasses - Adam and Eve.ts
+++ TypeScript/8 kyu/Basic subclasses - Adam and Eve.ts
@@ -1,19 +1,29 @@
-class God{
+export class God{
     /**
      * @returns Human[]
      */
       static create(){
         // code
+        let Adam = new Man(true, 'woman')
+        let Eva = new Woman(false, 'man')
+        return [Adam, Eva]
       }
 }
 
-class Human {
+export class Human {
     _tall: boolean
     constructor(tall: boolean) {
         this._tall = tall
     }
 }
-class Man extends Human {
+export class Man extends Human {
+    _likes: 'woman' | 'man';
+    constructor(tall: boolean, likes: 'woman' | 'man') {
+        super(tall);
+        this._likes = likes
+    }
+}
+export class Woman extends Human {
     _likes: 'woman' | 'man';
     constructor(tall: boolean, likes: 'woman' | 'man') {
         super(tall);
